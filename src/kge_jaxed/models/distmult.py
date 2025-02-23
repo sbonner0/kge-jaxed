df7c58a1edcc43f9a3b8317fe33c888cc410fce8
Stephen Bonner <sbonner0@users.noreply.github.com>
2025-02-23 20:36:49 +0000

    add DistMult interaction function (#7)

---
+++ src/kge_jaxed/models/distmult.py
@@ -0,0 +1,12 @@
+import jax.numpy as jnp
+from jax import Array
+
+from kge_jaxed.models.base_kge import BaseKGE
+
+
+class DistMult(BaseKGE):
+    def __init__(self, num_entities: int, num_relations: int, embedding_dim: int) -> None:
+        super().__init__(num_entities, num_relations, embedding_dim)
+
+    def interaction_function(self, h: Array, r: Array, t: Array) -> Array:
+        return jnp.sum(h * r * t, axis=1)
